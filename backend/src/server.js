fa9367ab099cea2f66d70d8986c4cf276e0b8ff8
mythri-5529 <24ai-mythri5529@yit.edu.in>
2026-04-29 08:31:39 +0530

    Done

--- backend/src/server.js
+++ backend/src/server.js
@@ -47,7 +47,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
 httpServer.listen(PORT, () => {
   console.log(`Backend server running on port ${PORT}`);
 });
